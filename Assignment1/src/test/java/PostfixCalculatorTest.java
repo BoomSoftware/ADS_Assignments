2c9a6b4437d34fe3f21bb629593413268eced11c
PolishHunter <o.szeferpldk@gmail.com>
2021-03-09 22:37:01 +0100

    I'm going to fill out the other methods in the upcoming days...

--- Assignment1/src/test/java/PostfixCalculatorTest.java
+++ Assignment1/src/test/java/PostfixCalculatorTest.java
@@ -1,9 +1,30 @@
+import Exceptions.EmptyListException;
 import org.junit.jupiter.api.Test;
+import stack.LinkedList;
+import stack.LinkedStack;
+import stack.Node;
+
+import java.awt.*;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PostfixCalculatorTest {
 
+    // Unit Test for the LinkedList in stack package.
     @Test
-    void test(){
+    public <T> void testIsEmpty(){
+        LinkedList<T> linkedList = new LinkedList<>();
 
+        int result = linkedList.size();
+
+        assertEquals(10, 5, result);
+    }
+
+    @Test
+    public <T> void testSize(){
+        LinkedList<T> linkedList = new LinkedList<>();
+        assertTrue(linkedList.isEmpty());
+        assertFalse(linkedList.isEmpty());
     }
 }
