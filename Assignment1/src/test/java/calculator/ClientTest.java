3f4a825d30363465aa1a381720dbc5266445a6cb
Estera <dumi_estera@yahoo.com>
2021-03-17 22:07:13 +0100

    Client

--- Assignment1/src/test/java/calculator/ClientTest.java
+++ Assignment1/src/test/java/calculator/ClientTest.java
@@ -1,21 +1,30 @@
 package calculator;
 
+import Exceptions.EmptyListException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import stack.LinkedList;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
 
     private Client client;
+    private CalculatorVisitor calculatorVisitor;
 
     @BeforeEach
     void setUpCalculatorVisitor()
     {
-
+         client = new Client(calculatorVisitor);
     }
 
     @Test
     void testEvaluateExpression() {
+
+        ArrayList<Token> tokens = new ArrayList<>();
+        Assertions.assertThrows(EmptyListException.class, () -> client.evaluateExpression(tokens));
     }
 }
